ce28d50f49b6b4bcb8a9f66bd2ff53c7c7f072df
niebowen <18656957051@163.com>
2018-03-13 17:04:36 +0800

    add some exercises

---
+++ C++_Optimization_Practice/Vector Storage/initializer.cpp
@@ -0,0 +1,11 @@
+
+#include "initializer.hpp"
+
+using namespace std;
+
+vector < vector<int> > initializer(int rows, int cols, int initial_value) {
+    
+    vector <vector<int> > matrix(rows, vector<int>(cols, initial_value));
+    return matrix;
+    
+}
---
+++ C++_Optimization_Practice/Vector Storage/reserved.hpp
@@ -0,0 +1,9 @@
+
+#ifndef reserved_hpp
+#define reserved_hpp
+
+#include <vector>
+
+std::vector< std::vector<int> > reserved(int rows, int cols, int initial_value);
+
+#endif /* reserved_hpp */
